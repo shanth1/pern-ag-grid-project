22018b7730020a31dbbbe884b068caec49c1dd80
Denis Beresnev <shanthi.bunch@gmail.com>
2023-09-04 11:01:40 +0300

    axios with basic ag-grid set up

--- client/src/layouts/tables/index.js
+++ client/src/layouts/tables/index.js
@@ -8,26 +8,55 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { getAllOffices } from "api/office";
+import { useEffect, useMemo, useState } from "react";
 
 function Tables() {
-    const rowData = [
-        { name: "Denis", surname: "Beresnev", age: 22 },
-        { name: "Mark", surname: "Orlov", age: 19 },
-    ];
+    const [rowData, setRowData] = useState([]);
     const columnDefs = [
-        { field: "name" },
-        { field: "surname" },
-        { field: "age" },
+        { field: "country" },
+        { field: "city" },
+        {
+            field: "square",
+            comparator: (valueA, valueB) => valueA - valueB,
+        },
+        {
+            field: "squareRentPrice",
+            comparator: (valueA, valueB) => valueA - valueB,
+        },
+        { field: "openingDate" },
+        { field: "createdAt" },
     ];
+
+    const defaultColDefs = useMemo(
+        () => ({ sortable: true, filter: true }),
+        [],
+    );
+
+    const cellClickListener = (e) => {
+        console.log("click", e);
+    };
+
+    useEffect(() => {
+        getAllOffices().then(({ data }) => {
+            setRowData(data);
+        });
+    }, []);
 
     return (
         <DashboardLayout>
             <DashboardNavbar />
-            <MDBox py={3}>
-                <div className="ag-theme-alpine" style={{ height: 500 }}>
-                    <AgGridReact rowData={rowData} columnDefs={columnDefs} />
-                </div>
-            </MDBox>
+            <MDBox mb={3} />
+            <div className="ag-theme-alpine" style={{ height: 500 }}>
+                <AgGridReact
+                    rowData={rowData}
+                    rowSelection="single"
+                    onCellClicked={cellClickListener}
+                    columnDefs={columnDefs}
+                    defaultColDef={defaultColDefs}
+                    animateRows={true}
+                />
+            </div>
         </DashboardLayout>
     );
 }
