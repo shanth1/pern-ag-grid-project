bc1d638a94916257d8989b3ab6aff62ae42b3224
Denis Beresnev <shanthi.bunch@gmail.com>
2023-09-07 22:42:00 +0300

    sequelize query, limit and offset in sql

--- server/src/controllers/officeController.js
+++ server/src/controllers/officeController.js
@@ -1,8 +1,18 @@
-const Office = require("../models/Office");
+const sequelize = require("../config/db");
+const { QueryTypes } = require("sequelize");
 
 class OfficeController {
     async getAll(req, res) {
-        const data = await Office.findAll();
+        const { startRow, endRow } = req.body;
+        console.log(startRow, "-", endRow);
+        const data = await sequelize.query(
+            `SELECT id, country, city, square, "squareRentPrice", "openingDate", "createdAt", "updatedAt" FROM public.office LIMIT ${
+                endRow - startRow
+            } OFFSET ${startRow};`,
+            {
+                type: QueryTypes.SELECT,
+            },
+        );
         res.json(data);
     }
 }
