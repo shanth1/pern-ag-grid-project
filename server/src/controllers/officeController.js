949740d775f4c041f77b0778c2984dcb00264a1b
Denis Beresnev <shanthi.bunch@gmail.com>
2023-09-08 21:31:10 +0300

    server validation and error exception

--- server/src/controllers/officeController.js
+++ server/src/controllers/officeController.js
@@ -3,17 +3,39 @@ const { QueryTypes } = require("sequelize");
 
 class OfficeController {
     async getAll(req, res) {
-        const { startRow, endRow } = req.body;
-        console.log(startRow, "-", endRow);
-        const data = await sequelize.query(
-            `SELECT id, country, city, square, "squareRentPrice", "openingDate", "createdAt", "updatedAt" FROM public.office LIMIT ${
-                endRow - startRow
-            } OFFSET ${startRow};`,
-            {
-                type: QueryTypes.SELECT,
-            },
-        );
-        res.json(data);
+        const { startRow, endRow, sortModel, filterModel } = req.query;
+
+        const fields = `id, country, city, square, "squareRentPrice", "openingDate", "createdAt", "updatedAt"`;
+        const table = "public.office";
+
+        const sorting = sortModel
+            ? `ORDER BY "${sortModel[0].colId}" ${sortModel[0].sort}`
+            : "";
+
+        const filtration = getFilterString(filterModel);
+
+        try {
+            const offices = await sequelize.query(
+                `SELECT ${fields} FROM ${table} ${filtration} ${sorting} LIMIT ${
+                    endRow - startRow
+                } OFFSET ${startRow};`,
+                {
+                    type: QueryTypes.SELECT,
+                },
+            );
+            const lengthResponse = await sequelize.query(
+                `SELECT COUNT(*) FROM ${table} ${filtration};`,
+                {
+                    type: QueryTypes.SELECT,
+                },
+            );
+            return res.json({
+                offices,
+                length: Number(lengthResponse[0].count),
+            });
+        } catch (error) {
+            return res.status(500).send(error.message);
+        }
     }
 }
 
